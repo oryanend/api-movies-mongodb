253ea7ccbad49efe29af575acb0f1c5d1ffa43f9
oryanend <ryanyamim@gmail.com>
2025-02-25 09:27:33 -0300

    First Commit (Initializing Project)

---
+++ src/main/java/com/oryanend/mongodb_movies/MongodbMoviesApplication.java
@@ -0,0 +1,13 @@
+package com.oryanend.mongodb_movies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbMoviesApplication.class, args);
+	}
+
+}
